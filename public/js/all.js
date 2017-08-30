26a2e265de9c5c7cf7dff9236a2e12682649ae2d
SLP_WEBDev <SLP_WEBDev>
2017-08-30 11:57:39 +0800

    Adding crud nodejs using ejs

---
+++ public/js/all.js
@@ -0,0 +1,12 @@
+function addCustomer(){
+            
+    window.location.href = '/customers';
+}
+function addUser(){
+            
+    window.location.href = '/customers/add';
+}
+function cancelAdd(){
+    
+    window.location.href = '/customers';
+}
